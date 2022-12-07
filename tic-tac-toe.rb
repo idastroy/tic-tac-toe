100b79a6943219e301d4d192197007dda5993fe3
Ida Glen <idamglen@gmail.com>
2022-12-07 11:31:40 -0500

    broke game play into different methods for organization

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -5,7 +5,6 @@ module Board
   @@options = [1, 2, 3, 
                4, 5, 6, 
                7, 8, 9]
-
   def make_grid
     puts " ", @@top_row.join, @@middle_row.join, @@bottom_row.join, " "
   end
@@ -21,6 +20,7 @@ class Game
   @@player1_turn = true
 
   def initialize
+    puts @@winning_combos
     puts "Enter the first player's name."
     @player1 = gets.chomp
     puts " ", "Hello #{@player1}, you are #{@@symbols[@@player]}", " "
@@ -44,170 +44,175 @@ class Game
         @@player1_turn = true
         @@player += 1
       end
-      
+      choose_position
+      update_grid
+      declare_winner
+    end
+  end
+  
+  def choose_position
+    @player_selection = gets.chomp.to_i
+    if !@@options.include?(@player_selection)
+      puts "Oops, that's not an option. Try again!"
       @player_selection = gets.chomp.to_i
-      if !@@options.include?(@player_selection)
-        puts "Oops, that's not an option. Try again!"
-        @player_selection = gets.chomp.to_i
-      else 
-        puts "You picked #{@player_selection}"
-        @@options.delete(@player_selection)
-        @@round += 1 
-      end 
-      
-
-      ##Change grid when player selects 1-9
-      if @player_selection == 1
-        @@top_row[0] = @@symbols[@@player]
-      end
-
-      if @player_selection == 2
-        @@top_row[2] = @@symbols[@@player]
-      end
-
-      if @player_selection == 3
-        @@top_row[4] = @@symbols[@@player]
-      end
-
-      if @player_selection == 4
-        @@middle_row[0] = @@symbols[@@player]
-      end
-
-      if @player_selection == 5
-        @@middle_row[2] = @@symbols[@@player]
-      end
-
-      if @player_selection == 6
-        @@middle_row[4] = @@symbols[@@player]
-      end
-
-      if @player_selection == 7
-        @@bottom_row[0] = @@symbols[@@player]
-      end
-
-      if @player_selection == 8
-        @@bottom_row[2] = @@symbols[@@player]
-      end
-
-      if @player_selection == 9
-        @@bottom_row[4] = @@symbols[@@player]
-      end
-
-      ##Declare winner for horizontal
-
-      if @@top_row[0] == "X" && @@top_row[2] == "X" && @@top_row[4] == "X"
-        make_grid
-        puts "#{@player1} wins!"
-        break
-      end
-
-      if @@top_row[0] == "O" && @@top_row[2] == "O" && @@top_row[4] == "O"
-        make_grid
-        puts "#{@player2} wins!"
-        break
-      end
-
-      if @@middle_row[0] == "X" && @@middle_row[2] == "X" && @@middle_row[4] == "X"
-        make_grid
-        puts "#{@player1} wins!"
-        break
-      end
-
-      if @@middle_row[0] == "O" && @@middle_row[2] == "O" && @@middle_row[4] == "O"
-        make_grid
-        puts "#{@player2} wins!"
-        break
-      end
-
-      if @@bottom_row[0] == "X" && @@bottom_row[2] == "X" && @@bottom_row[4] == "X"
-        make_grid
-        puts "#{@player1} wins!"
-        break
-      end
-
-      if @@bottom_row[0] == "O" && @@bottom_row[2] == "O" && @@bottom_row[4] == "O"
-        make_grid
-        puts "#{@player2} wins!"
-        break
-      end
-
-      ##Declare winner for vertical
-
-      if @@top_row[0] == "X" && @@middle_row[0] == "X" && @@bottom_row[0] == "X"
-        make_grid
-        puts "#{@player1} wins!"
-        break
-      end
-
-      if @@top_row[0] == "O" && @@middle_row[0] == "O" && @@bottom_row[0] == "O"
-        make_grid
-        puts "#{@player2} wins!"
-        break
-      end
-
-      if @@top_row[2] == "X" && @@middle_row[2] == "X" && @@bottom_row[2] == "X"
-        make_grid
-        puts "#{@player1} wins!"
-        break
-      end
-
-      if @@top_row[2] == "O" && @@middle_row[2] == "O" && @@bottom_row[2] == "O"
-        make_grid
-        puts "#{@player2} wins!"
-        break
-      end
-
-      if @@top_row[4] == "X" && @@middle_row[4] == "X" && @@bottom_row[4] == "X"
-        make_grid
-        puts "#{@player1} wins!"
-        break
-      end
-
-      if @@top_row[4] == "O" && @@middle_row[4] == "O" && @@bottom_row[4] == "O"
-        make_grid
-        puts "#{@player2} wins!"
-        break
-      end
-
-      ##Declare winner for diagonal
-
-      if @@top_row[0] == "X" && @@middle_row[2] == "X" && @@bottom_row[4] == "X"
-        make_grid
-        puts "#{@player1} wins!"
-        break
-      end
-
-      if @@top_row[0] == "O" && @@middle_row[2] == "O" && @@bottom_row[4] == "O"
-        make_grid
-        puts "#{@player2} wins!"
-        break
-      end
-
-      if @@top_row[4] == "X" && @@middle_row[2] == "X" && @@bottom_row[0] == "X"
-        make_grid
-        puts "#{@player1} wins!"
-        break
-      end
-
-      if @@top_row[4] == "O" && @@middle_row[2] == "O" && @@bottom_row[0] == "O"
-        make_grid
-        puts "#{@player2} wins!"
-        break
-      end
-
-
+    else 
+      puts "You picked #{@player_selection}"
+      @@options.delete(@player_selection)
+      @@round += 1 
     end 
+  end
+      
+  def update_grid
+    ##Change grid when player selects 1-9
+    if @player_selection == 1
+      @@top_row[0] = @@symbols[@@player]
+    end
 
-      ##Declare a tie
+    if @player_selection == 2
+      @@top_row[2] = @@symbols[@@player]
+    end
 
-      if @@round == 9
-        make_grid
-        puts "It's a tie!"
-      end
+    if @player_selection == 3
+      @@top_row[4] = @@symbols[@@player]
+    end
 
-    
+    if @player_selection == 4
+      @@middle_row[0] = @@symbols[@@player]
+    end
+
+    if @player_selection == 5
+      @@middle_row[2] = @@symbols[@@player]
+    end
+
+    if @player_selection == 6
+      @@middle_row[4] = @@symbols[@@player]
+    end
+
+    if @player_selection == 7
+      @@bottom_row[0] = @@symbols[@@player]
+    end
+
+    if @player_selection == 8
+      @@bottom_row[2] = @@symbols[@@player]
+    end
+
+    if @player_selection == 9
+      @@bottom_row[4] = @@symbols[@@player]
+    end
   end
 
+      
+  def declare_winner
+    ##Declare winner for horizontal
+
+    if @@top_row[0] == "X" && @@top_row[2] == "X" && @@top_row[4] == "X"
+      make_grid
+      puts "#{@player1} wins!"
+      exit
+    end
+
+    if @@top_row[0] == "O" && @@top_row[2] == "O" && @@top_row[4] == "O"
+      make_grid
+      puts "#{@player2} wins!"
+      exit
+    end
+
+    if @@middle_row[0] == "X" && @@middle_row[2] == "X" && @@middle_row[4] == "X"
+      make_grid
+      puts "#{@player1} wins!"
+      exit
+    end
+
+    if @@middle_row[0] == "O" && @@middle_row[2] == "O" && @@middle_row[4] == "O"
+      make_grid
+      puts "#{@player2} wins!"
+      exit
+    end
+
+    if @@bottom_row[0] == "X" && @@bottom_row[2] == "X" && @@bottom_row[4] == "X"
+      make_grid
+      puts "#{@player1} wins!"
+      exit
+    end
+
+    if @@bottom_row[0] == "O" && @@bottom_row[2] == "O" && @@bottom_row[4] == "O"
+      make_grid
+      puts "#{@player2} wins!"
+      exit
+    end
+
+    ##Declare winner for vertical
+
+    if @@top_row[0] == "X" && @@middle_row[0] == "X" && @@bottom_row[0] == "X"
+      make_grid
+      puts "#{@player1} wins!"
+      exit
+    end
+
+    if @@top_row[0] == "O" && @@middle_row[0] == "O" && @@bottom_row[0] == "O"
+      make_grid
+      puts "#{@player2} wins!"
+      exit
+    end
+
+    if @@top_row[2] == "X" && @@middle_row[2] == "X" && @@bottom_row[2] == "X"
+      make_grid
+      puts "#{@player1} wins!"
+      exit
+    end
+
+    if @@top_row[2] == "O" && @@middle_row[2] == "O" && @@bottom_row[2] == "O"
+      make_grid
+      puts "#{@player2} wins!"
+      exit
+    end
+
+    if @@top_row[4] == "X" && @@middle_row[4] == "X" && @@bottom_row[4] == "X"
+      make_grid
+      puts "#{@player1} wins!"
+      exit
+    end
+
+    if @@top_row[4] == "O" && @@middle_row[4] == "O" && @@bottom_row[4] == "O"
+      make_grid
+      puts "#{@player2} wins!"
+      exit
+    end
+
+    ##Declare winner for diagonal
+
+    if @@top_row[0] == "X" && @@middle_row[2] == "X" && @@bottom_row[4] == "X"
+      make_grid
+      puts "#{@player1} wins!"
+      exit
+    end
+
+    if @@top_row[0] == "O" && @@middle_row[2] == "O" && @@bottom_row[4] == "O"
+      make_grid
+      puts "#{@player2} wins!"
+      exit
+    end
+
+    if @@top_row[4] == "X" && @@middle_row[2] == "X" && @@bottom_row[0] == "X"
+      make_grid
+      puts "#{@player1} wins!"
+      exit
+    end
+
+    if @@top_row[4] == "O" && @@middle_row[2] == "O" && @@bottom_row[0] == "O"
+      make_grid
+      puts "#{@player2} wins!"
+      exit
+    end
+
+    ##Declare a tie
+
+    if @@round == 9
+      make_grid
+      puts "It's a tie!"
+    end
+
+  end
 
 end
 
